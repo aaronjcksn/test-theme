ec2e84f47396909d2c4e0466f3047d069aa8e159
Aaron Jackson <aaron.jackson@red8interactive.com>
2015-07-17 15:20:54 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package boiler
+ */
+?>
+
+	<footer id="global_footer" class="site_footer">
+		<div class="container">
+			<p class="copy">&copy; <?php echo date('Y'); ?> <?php bloginfo('site_title'); ?></p>
+		</div>
+	</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+
+	var localDev = true;
+
+	if(localDev == true) {
+		loadReload();
+	}
+	
+});
